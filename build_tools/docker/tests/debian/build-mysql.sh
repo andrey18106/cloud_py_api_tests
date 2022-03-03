cb1c7e51a0832b20d39031c0c1a66d17afb5c466
Andrey Borysenko <andrey18106x@gmail.com>
2022-03-03 17:08:41 +0200

    Minor fixes

--- build_tools/docker/tests/debian/build-mysql.sh
+++
@@ -1,9 +0,0 @@
-docker build -t cpa_debian_mysql_test ./debian/ \
---build-arg OS=debian \
---build-arg VER=10.11 \
---build-arg BASE_IMAGE=debian:10.11 \
---build-arg ENTRY_POINT=debian.integration.sh \
---build-arg NEXTCLOUD_VERSION=v22.2.5 \
---build-arg NC_CREATE_USER_SQL=mysql_create_user.sql \
---build-arg PHP_VERSION=7.4 \
---build-arg DB_TYPE=mysql
--- build_tools/docker/tests/debian/build-pgsql.sh
+++
@@ -1,7 +0,0 @@
-docker build -t cpa_debian_pgsql_test ./debian/ \
---build-arg BASE_IMAGE=debian:10.11 \
---build-arg ENTRY_POINT=debian.integration.sh \
---build-arg NEXTCLOUD_VERSION=v23.0.2 \
---build-arg NC_CREATE_USER_SQL=pgsql_create_user.sql \
---build-arg PHP_VERSION=7.4 \
---build-arg DB_TYPE=pgsql
